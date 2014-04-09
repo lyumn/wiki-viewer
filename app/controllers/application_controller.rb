ea5fa5c52d5f3cd23a4e668dda80989e158e5adf
lyumn <me@lyumn.net>
2014-04-09 04:49:44 +0000

    general wiki supported

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_charsets 
+
+# Configuring charset to UTF-8 
+def configure_charsets 
+   headers["Content-Type"] = "text/html; charset=UTF-8"     
+end 
 end
--- app/controllers/wiki_viewer_controller.rb
+++ app/controllers/wiki_viewer_controller.rb
@@ -1,17 +1,32 @@
 require 'open-uri'
+require 'nokogiri'
 class WikiViewerController < ApplicationController
   def home
   end
   
   def source
-	link = "http://en.wikisource.org/wiki/"+params[:q]+"?action=render"
-	@src = Nokogiri::HTML(open(link))
+
+	@src = "hello"
+	link = "http://"+params[:lang]+"."+params[:wiki]+"."+params[:title]+"?action=render"	
+	html = open(link)
+	puts html
+	s = Nokogiri::HTML(html.read)
+	s.encoding = 'UTF-8'
+	@src = s.to_s
+	path = root_path+'/source/'
+	@src.gsub!('//'+params[:lang]+"."+params[:wiki]+".org/wiki/", '/source/'+params[:lang]+"/"+params[:wiki]+"/org/wiki/")	
   end
   
   def search
-	link = "http://en.wikisource.org/wiki/"+params[:q]+"?action=render"
-	@s = (Nokogiri::HTML(open(link))).to_s
-	@s.gsub!('//en.wikisource.org/wiki/', '../source/')
+	link = params[:q].split('.')
+	lang = link[0]
+	wiki = link[1]
+	title = link[2]
+	redirect_to :action => 'source', :lang => lang, :wiki => wiki, :title => title
+	#link = "http://en.wikisource.org/wiki/"+params[:q]+"?action=render"
+	#link = "https://en.wikisource.org/w/index.php?search="+params[:q]
+	#@search = (Nokogiri::HTML(open(link))).to_s
+	#@search.gsub!('//en.wikisource.org/wiki/', '/source/')
   end
   
 end
